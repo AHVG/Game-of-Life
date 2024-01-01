eada93fdc8261ae49b5504ea7a0aed0f5523bd02
Augusto H. V. Guerner <augusto.h.v.guerner@gmail.com>
2024-01-01 19:51:59 -0300

    Mostrando o grid e lidando com quit do usuário

--- src/app.py
+++ src/app.py
@@ -1,13 +1,36 @@
-
+import pygame 
+import constants
 
 class App:
 
     def __init__(self) -> None:
-        self.__running = True
+        self.__running: bool = True
+        self.__grid: list[list[bool]] = [[False for _ in range(constants.NUMBER_OF_SQUARES)] for _ in range(constants.NUMBER_OF_SQUARES)]
+        self.__surface = pygame.display.set_mode((600, 600)) 
 
     def is_running(self) -> bool:
         return self.__running
 
+    def handle_render(self) -> None:
+        
+        for line in range(constants.NUMBER_OF_SQUARES):
+            for column in range(constants.NUMBER_OF_SQUARES):
+                pygame.draw.rect(self.__surface,
+                                 constants.SQUARE_COLOR, 
+                                 pygame.Rect(column * constants.SQUARE_HEIGHT, 
+                                             line * constants.SQUARE_WIDTH, 
+                                             constants.SQUARE_WIDTH, 
+                                             constants.SQUARE_HEIGHT)) 
+        
+        pygame.display.flip()
+
+    def handle_events(self) -> None:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.__running = False
+
     def run(self) -> None:
+        pygame.init()
         while self.is_running():
-            print("TODO...")
+            self.handle_events()
+            self.handle_render()
