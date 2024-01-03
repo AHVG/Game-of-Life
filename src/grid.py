a8572d435325684cae9e32d1b19f20415f5d4aff
Augusto H. V. Guerner <augusto.h.v.guerner@gmail.com>
2024-01-03 16:15:58 -0300

    Fonts arrumadas

--- src/grid.py
+++ src/grid.py
@@ -8,7 +8,6 @@ import tile
 
 class Grid:
 
-    # TODO: arrumar as fontes
     # TODO: fazer tamanho variavel do grid para o usuario escolher
     # TODO: trocar square por tile
 
@@ -23,6 +22,20 @@ class Grid:
         self.__frames_per_second: int = 20
         self.__frames: int = 0
         self.__paused: bool = False
+
+        self.__helper_title_font = pygame.font.Font("./fonts/Minecraft.ttf", 50)
+        self.__helper_font = pygame.font.Font("./fonts/Minecraft.ttf", 21)
+
+        self.__speed_font = pygame.font.Font("./fonts/Minecraft.ttf", 25)
+
+        self.__helper_title = self.__helper_title_font.render('Keys', False, (180, 180, 180))
+        self.__arrow_up_text = self.__helper_font.render('Arrow UP - Increase simulation speed', False, (180, 180, 180))
+        self.__arrow_down_text = self.__helper_font.render('Arrow DOWN - Decrease simulation speed', False, (180, 180, 180))
+        self.__r_text = self.__helper_font.render('R - Reset the grid randomly', False, (180, 180, 180))
+        self.__p_text = self.__helper_font.render('P - Pause the grid', False, (180, 180, 180))
+        self.__click_text = self.__helper_font.render('Click - Switch tile state', False, (180, 180, 180))
+
+        self.__speed_text = self.__speed_font.render(f"Speed: {self.__frames_per_second} frames per second", False, (180, 180, 180))
 
 
     def __reset(self):
@@ -110,28 +123,15 @@ class Grid:
 
     def draw_at(self, surface: pygame.Surface) -> None:
 
-        helper_title_font = pygame.font.Font("./fonts/Minecraft.ttf", 50)
-        helper_font = pygame.font.Font("./fonts/Minecraft.ttf", 21)
+        surface.blit(self.__helper_title, (10, 30))
+        surface.blit(self.__arrow_up_text, (10, 110))
+        surface.blit(self.__arrow_down_text, (10, 150))
+        surface.blit(self.__r_text, (10, 190))
+        surface.blit(self.__p_text, (10, 230))
+        surface.blit(self.__click_text, (10, 270))
 
-        speed_font = pygame.font.Font("./fonts/Minecraft.ttf", 25)
-
-        helper_title = helper_title_font.render('Keys', False, (180, 180, 180))
-        arrow_up_text = helper_font.render('Arrow UP - Increase simulation speed', False, (180, 180, 180))
-        arrow_down_text = helper_font.render('Arrow DOWN - Decrease simulation speed', False, (180, 180, 180))
-        r_text = helper_font.render('R - Reset the grid randomly', False, (180, 180, 180))
-        p_text = helper_font.render('P - Pause the grid', False, (180, 180, 180))
-        click_text = helper_font.render('Click - Switch tile state', False, (180, 180, 180))
-
-        speed_text = speed_font.render(f"Speed: {self.__frames_per_second} frames per second", False, (180, 180, 180))
-
-        surface.blit(helper_title, (10, 30))
-        surface.blit(arrow_up_text, (10, 110))
-        surface.blit(arrow_down_text, (10, 150))
-        surface.blit(r_text, (10, 190))
-        surface.blit(p_text, (10, 230))
-        surface.blit(click_text, (10, 270))
-
-        surface.blit(speed_text, (10, 390))
+        self.__speed_text = self.__speed_font.render(f"Speed: {self.__frames_per_second} frames per second", False, (180, 180, 180))
+        surface.blit(self.__speed_text, (10, 390))
 
         for line in range(constants.NUMBER_OF_SQUARES):
             for column in range(constants.NUMBER_OF_SQUARES):
